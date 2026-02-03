aabbb6219da0f45ca47e0b1ca4aa3507f034aa19
Shakhsanam Imitova <shakhsanamimitova1@gmail.com>
2026-02-03 23:55:37 +0100

    .

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,7 +1,5 @@
-// src/db/drizzle.ts
 import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
-import * as schema from "./schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
@@ -9,7 +7,8 @@ if (!process.env.DATABASE_URL) {
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, // REQUIRED for Railway
+  ssl: { rejectUnauthorized: false },
 });
 
-export const db = drizzle(pool, { schema });
+export const db = drizzle(pool);
+
